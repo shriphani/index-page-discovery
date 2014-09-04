670a886ef7c6245f3e09f1f7216462fb900668bb
Shriphani Palakodety <shriphanip@gmail.com>
2014-09-04 15:44:14 -0400

    initial commit

---
+++ test/kba_index_discovery/core_test.clj
@@ -0,0 +1,7 @@
+(ns kba-index-discovery.core-test
+  (:require [clojure.test :refer :all]
+            [kba-index-discovery.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
