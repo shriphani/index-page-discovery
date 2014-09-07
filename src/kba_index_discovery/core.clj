5d947c14f69168e53b22afcec3efe695d0637cae
Shriphani Palakodety <shriphanip@gmail.com>
2014-09-07 03:17:53 -0400

    merge docs

--- src/kba_index_discovery/core.clj
+++ src/kba_index_discovery/core.clj
@@ -1,6 +1,46 @@
-(ns kba-index-discovery.core)
+(ns kba-index-discovery.core
+  (:require [clojure.java.io :as io]
+            [clojure.set :as clj-set])
+  (:import [java.io PushbackReader])
+  (:use [clojure.pprint :only [pprint]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn load-index-names-file
+  [a-file]
+  (let [rdr (-> a-file io/reader PushbackReader.)
+        records (take-while
+                 identity
+                 (repeatedly
+                  (fn []
+                    (try
+                      (read rdr)
+                      (catch Exception e nil)))))
+
+        res
+        (reduce
+         (fn [acc {uri        :uri
+                  index-name :index-name}]
+           (merge-with clj-set/union
+                       acc
+                       {uri (set index-name)}))
+         {}
+         records)
+
+        wrtr (-> (str a-file ".records")
+                 io/writer)]
+    (pprint res wrtr)))
+
+(defn merge-records
+  [records]
+  (reduce
+   (fn [acc r]
+     (let [rdr (-> r io/reader PushbackReader.)]
+      (merge-with clj-set/union
+                  acc
+                  (read rdr))))
+   {}
+   records))
+
+(defn -main
+  [& args]
+  (load-index-names-file
+   (first args)))
