e34d9238d3af30f02fa4473cde5e357bc8c171c5
Shriphani Palakodety <shriphanip@gmail.com>
2014-09-07 19:03:27 -0400

    added timeouts and fixes for downloading initial corpus

--- src/kba_index_discovery/core.clj
+++ src/kba_index_discovery/core.clj
@@ -1,6 +1,8 @@
 (ns kba-index-discovery.core
   (:require [clojure.java.io :as io]
-            [clojure.set :as clj-set])
+            [clojure.set :as clj-set]
+            [clojure.tools.cli :refer [parse-opts]]
+            [kba-index-discovery.download-landing-pages :as download-index-pages])
   (:import [java.io PushbackReader])
   (:use [clojure.pprint :only [pprint]]))
 
@@ -40,7 +42,16 @@
    {}
    records))
 
+(def cli-options
+  [[nil "--download-index-pages" "Download index pages and build a simple corpus"]])
+
 (defn -main
   [& args]
-  (load-index-names-file
-   (first args)))
+  (let [options (-> args (parse-opts cli-options) :options)]
+    (cond (:download-index-pages options)
+          (let [wrtr (io/writer "/bos/tmp19/spalakod/index-pages.clj")]
+            (download-index-pages/get-corpora wrtr))
+          
+          :else
+          (load-index-names-file
+           (first args)))))
